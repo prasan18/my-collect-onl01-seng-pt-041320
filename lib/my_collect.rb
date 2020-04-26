871e2212a36945442fb9df1aab4b819beba2f5b2
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-26 20:30:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
 
 
+def my_collect(array)
+    i = 0
+    languages = []
+    students = []
+    while i < array.length 
+   
+    
+  array== languages + students << yield(array[i])    
+     my_collect(students) do |students|
+            students.split(" ").first
+          end
+          my_collect(languages) do |languages|
+            languages.upcase
+          end 
+       i += 1 
+          end
+    array
+end
+ 
+ 
+     
